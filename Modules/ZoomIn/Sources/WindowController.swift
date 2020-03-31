334efc220d4d297854e25fa2e27c6b029fd8e9c3
Sam Soffes <sam@soff.es>
2020-03-30 22:07:31 -0700

    Organize

--- Modules/ZoomIn/Sources/WindowController.swift
+++ Modules/ZoomIn/Sources/WindowController.swift
@@ -1,6 +1,11 @@
 import AppKit
 
 final class WindowController: NSWindowController, NSWindowDelegate {
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        window?.center()
+    }
+
     func windowWillClose(_ notification: Notification) {
         NSApp.terminate(notification)
     }
