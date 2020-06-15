ef85f9b3647e7ef5b17635b287468848756767bb
Sebastian Hennig <hello@bastihennig.com>
2020-06-15 22:39:39 +0200

    Fixes closing Zooms new landing page soffes/ZoomIn#5

--- Modules/ZoomInExtension/Resources/script.js
+++ Modules/ZoomInExtension/Resources/script.js
@@ -1,8 +1,21 @@
 // Wait for the page to load
-document.addEventListener("DOMContentLoaded", function(event) {
+window.addEventListener("load", function(event) {
     // Get the URL from Zoom’s page
-    const url = document.getElementById('scheme_url')['value'];
-
+    let url;
+    let oldZoomPageElement = document.getElementById('scheme_url');
+    if (oldZoomPageElement) {
+        url = oldZoomPageElement['value'];
+    } else {
+        const selectors = document.querySelectorAll('[launch]');
+        if (selectors.length > 0) {
+            url = selectors[0].href;
+        }
+    }
+    
+    if (!url) {
+        return;
+    }
+    
     // Tell the native extension about the URL
     safari.extension.dispatchMessage('open', { url: url });
 });
